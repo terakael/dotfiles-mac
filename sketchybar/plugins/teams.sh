8a9024b0938e64c017f065ffa6e7ed30a6ee877d
Daniel Knezevic <daniel.knezevic@rakuten.com>
2026-06-01 07:45:24 +0900

    remove work-specific sketchybar plugins (belong in work overlay)

--- sketchybar/plugins/teams.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source "$HOME/.config/sketchybar/colors.sh"
-
-COUNT="${TEAMS_UNREAD_COUNT:-0}"
-
-sketchybar --set teams icon="󰻞" icon.color=$LAVENDER label="$COUNT" label.color=$TEXT1
